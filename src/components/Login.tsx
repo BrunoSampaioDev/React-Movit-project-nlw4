f9877727924feb95c1cbe2a827b968507e74550f
BrunoSampaioDev <sampaionascimento100@gmail.com>
2021-03-04 17:32:33 -0300

    Login via github finalizado

---
+++ src/components/Login.tsx
@@ -0,0 +1,7 @@
+import styles from '../styles/components/Login.module.css';
+
+export function Login(){
+    return (
+        <div className={styles.LoginPage}></div>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,46 +6,107 @@ import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ChallengeBox } from "../components/ChallengeBox";
 
+import { signIn, signOut, useSession } from 'next-auth/client'
+
 import styles from '../styles/pages/Home.module.css';
 import { CountdownProvider } from '../contexts/CountdouwnContext';
 import { ChallengeProvider } from '../contexts/ChallengesContext';
-
+import {FaGithub} from "react-icons/fa"
+import {Login} from '../components/Login';
 
 interface HomeProps {
-  level:number;
-  currentExpirience:number;
-  challengesComplited:number;
+  level: number;
+  currentExpirience: number;
+  challengesComplited: number;
 }
 
 export default function Home(props: HomeProps) {
+
+  const [session, loading] = useSession()
+
   return (
-    <ChallengeProvider 
-    level={props.level}
-    currentExpirience={props.currentExpirience}
-    challengesComplited={props.challengesComplited}
-    >
-      <div className={styles.container}>
-        <Head>
-          <title>Inicio | move.it</title>
-        </Head>
 
-        <ExpirienceBar />
+    <>
+      {!session && <>
+        <div className={styles.ContainerLogin}>
+          <div>
+            <img className={styles.simbolo} src="/icons/Simbolo.png"/>
+          </div>
 
-        <CountdownProvider>
-          <section>
-            <div>
-              <Profile />
-              <CompletedChallenges />
-              <Countdown />
-            </div>
+          <div className={styles.boxLogin}>
+            <img src="/icons/Logo.png"/>
+            <h3>Bem-Vindo</h3>
+            <button className={styles.btnLogin} onClick={() => signIn('github')}><FaGithub className={styles.iconGit}/>Entre com GitHub</button>
+            
+            
+          </div>
+        </div>
+        </>
+      }
 
-            <div>
-              <ChallengeBox />
-            </div>
-          </section>
-        </CountdownProvider>
-      </div>
-      </ChallengeProvider>
+
+
+      {session && <>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+        <ChallengeProvider
+          level={props.level}
+          currentExpirience={props.currentExpirience}
+          challengesComplited={props.challengesComplited}
+        >
+          <div className={styles.container}>
+            <Head>
+              <title>Inicio | move.it</title>
+            </Head>
+
+            <ExpirienceBar />
+
+            <CountdownProvider>
+              <section>
+                <div>
+                  <Profile />
+                  <CompletedChallenges />
+                  <Countdown />
+                </div>
+
+                <div>
+                  <ChallengeBox />
+                </div>
+              </section>
+            </CountdownProvider>
+          </div>
+        </ChallengeProvider>
+      </>}
+    </>
+
+
+    /* <ChallengeProvider 
+        level={props.level}
+        currentExpirience={props.currentExpirience}
+        challengesComplited={props.challengesComplited}
+        >
+          <div className={styles.container}>
+            <Head>
+              <title>Inicio | move.it</title>
+            </Head>
+    
+            <ExpirienceBar />
+    
+            <CountdownProvider>
+              <section>
+                <div>
+                  <Profile />
+                  <CompletedChallenges />
+                  <Countdown />
+                </div>
+    
+                <div>
+                  <ChallengeBox />
+                </div>
+              </section>
+            </CountdownProvider>
+          </div>
+          </ChallengeProvider> */
   )
 }
 
@@ -59,8 +120,8 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
     props: {
 
       level: Number(level),
-      currentExpirience:Number(currentExpirience),
-      challengesComplited:Number(challengesComplited),
+      currentExpirience: Number(currentExpirience),
+      challengesComplited: Number(challengesComplited),
 
     }
 
